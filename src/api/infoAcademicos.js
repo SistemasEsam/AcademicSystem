88eead51e702c62ec9f248d72e49a9bf9e4c04b7
Royer Angulo <royer.angulo@esam.edu.bo>
2024-10-21 18:00:37 -0400

    subida del proyecto

---
+++ src/api/infoAcademicos.js
@@ -0,0 +1,51 @@
+export const infoAcademicos = [
+  {
+    id: 1,
+    nombres: "Alejandra Lili",
+    apellido_paterno: "Rodriguez",
+    apellido_materno: "Flores",
+    correo: "alejandra.rodriguez@esam.edu.bo",
+  },
+  {
+    id: 2,
+    nombres: "Alejandra",
+    apellido_paterno: "Camacho",
+    apellido_materno: "Ferrer",
+    correo: "alejandra.camacho@esam.edu.bo",
+  },
+  {
+    id: 3,
+    nombres: "María Celeste",
+    apellido_paterno: "Aparicio",
+    apellido_materno: "Mogro",
+    correo: "maria.aparicio@esam.edu.bo",
+  },
+  {
+    id: 4,
+    nombres: "Camila",
+    apellido_paterno: "Torrico",
+    apellido_materno: "Montaño",
+    correo: "camila.torrico@esam.edu.bo",
+  },
+  {
+    id: 5,
+    nombres: "Rosemary",
+    apellido_paterno: "Lopez",
+    apellido_materno: "Zapata",
+    correo: "rosemary.lopez@esam.edu.bo",
+  },
+  {
+    id: 6,
+    nombres: "María Olurcia",
+    apellido_paterno: "Maire",
+    apellido_materno: "Alcocer",
+    correo: "maria.maire@esam.edu.bo",
+  },
+  {
+    id: 7,
+    nombres: "Anahí Daniela",
+    apellido_paterno: "Vega",
+    apellido_materno: "Rojas",
+    correo: "anahi.vega@esam.edu.bo",
+  },
+];
