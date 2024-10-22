8af20c4d8dd33a9912d119081844e9c60d12b5c7
royerangulo <royer.angulo@esam.edu.bo>
2024-10-22 09:31:17 -0400

    Subida del proyecto

---
+++ src/api/variablesCardLink.js
@@ -0,0 +1,32 @@
+export const variablesCardLink = [
+  {
+    titulo: "perfil personal",
+    imagen: "icono imagen",
+    href: "/perfil/",
+  },
+  {
+    titulo: "registro de programas",
+    imagen: "icono imagen",
+    href: "/registro-programas/",
+  },
+  {
+    titulo: "docentes",
+    imagen: "icono imagen",
+    href: "/docentes/",
+  },
+  {
+    titulo: "postulantes",
+    imagen: "icono imagen",
+    href: "/postulantes/",
+  },
+  {
+    titulo: "programas",
+    imagen: "icono imagen",
+    href: "/programas/",
+  },
+  {
+    titulo: "certificaciones",
+    imagen: "icono imagen",
+    href: "/certificaciones/",
+  },
+];
