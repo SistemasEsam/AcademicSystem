191a5fdecc2fa9bbbf4c87b3299b5eb69e9e32ea
Alan Jimenez Arandia <aljiar23@gmail.com>
2024-10-29 10:48:32 -0400

    Actualizar API Interna Programas

--- src/api/infoModulosPrueba.js
+++ src/api/infoModulosPrueba.js
@@ -1,66 +1,208 @@
-export const infoModulos = [
-    {
-      id: 0,
-      nombre: "Modulo 0",
-      docente: "Docente 0",
-      apellido_materno: "Flores",
-      correo: "alejandra.rodriguez@esam.edu.bo",
-    },
-    {
-      id: 1,
-      nombres: "Modulo 1",
-      docente: "Docente 1",
-      apellido_paterno: "Camacho",
-      apellido_materno: "Ferrer",
-      correo: "alejandra.camacho@esam.edu.bo",
-    },
-    {
-      id: 2,
-      nombres: "Modulo 2",
-      docente: "Docente 2",
-      apellido_paterno: "Aparicio",
-      apellido_materno: "Mogro",
-      correo: "maria.aparicio@esam.edu.bo",
-    },
-    {
-      id: 3,
-      nombres: "Modulo 3",
-      docente: "Docente 3",
-      apellido_paterno: "Torrico",
-      apellido_materno: "Montaño",
-      correo: "camila.torrico@esam.edu.bo",
-    },
-    {
-      id: 4,
-      nombres: "Modulo 4",
-      docente: "Docente 4",
-      apellido_paterno: "Lopez",
-      apellido_materno: "Zapata",
-      correo: "rosemary.lopez@esam.edu.bo",
-    },
-    {
-      id: 5,
-      nombres: "Modulo 5",
-      docente: "Docente 5",
-      apellido_paterno: "Maire",
-      apellido_materno: "Alcocer",
-      correo: "maria.maire@esam.edu.bo",
-    },
-    {
-      id: 6,
-      nombres: "Modulo 6",
-      docente: "Docente 6",
-      apellido_paterno: "Vega",
-      apellido_materno: "Rojas",
-      correo: "anahi.vega@esam.edu.bo",
-    },
-    {
-        id: 7,
-        nombres: "Modulo 7",
-        docente: "Docente 7",
-        apellido_paterno: "Vega",
-        apellido_materno: "Rojas",
-        correo: "anahi.vega@esam.edu.bo",
+export const infoProgramas = [
+  {
+    id_programa: "M-4090",
+    modulos: [
+      {
+        id: "M-4090-M0",
+        nombre: "Gestion de tecnologias de la informacion",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
       },
-  ];
-  
+      {
+        id: "M-4090-M1",
+        nombre: "Análisis, Descripción Y Valoración De Puestos De Trabajo Por Competencias",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4090-M2",
+        nombre: "Nuevas Tendencias En Reclutamiento Y Selección En La Valoracion Del Talento",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4090-M3",
+        nombre: "Formación Y Evaluación De Desempeño Del Talento En Gestión Por Competencias",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio:"19:00",
+        hora_fin:"22:00",
+        fecha_de_clases:{
+          sesion_1:"29/10/2024",
+          sesion_2:"5/11/2024",
+          sesion_3:"12/11/2024",
+          sesion_4:"19/10/2024",
+        },
+        contenido_minimo:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      }
+    ]
+  },
+  {
+    id_programa: "M-4508",
+    modulos: [
+      {
+        id: "M-4508-M0",
+        nombre: "Gestion de tecnologias de la informacion",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4508-M1",
+        nombre: "Análisis, Descripción Y Valoración De Puestos De Trabajo Por Competencias",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4508-M2",
+        nombre: "Nuevas Tendencias En Reclutamiento Y Selección En La Valoracion Del Talento",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4508-M3",
+        nombre: "Formación Y Evaluación De Desempeño Del Talento En Gestión Por Competencias",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio:"19:00",
+        hora_fin:"22:00",
+        fecha_de_clases:{
+          sesion_1:"29/10/2024",
+          sesion_2:"5/11/2024",
+          sesion_3:"12/11/2024",
+          sesion_4:"19/10/2024",
+        },
+        contenido_minimo:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      }
+    ]
+  },
+  {
+    id_programa: "M-4493",
+    modulos: [
+      {
+        id: "M-4493-M0",
+        nombre: "Gestion de tecnologias de la informacion",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4493-M1",
+        nombre: "Análisis, Descripción Y Valoración De Puestos De Trabajo Por Competencias",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4493-M2",
+        nombre: "Nuevas Tendencias En Reclutamiento Y Selección En La Valoracion Del Talento",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio: "19:00",
+        hora_fin: "22:00",
+        fecha_de_clases: {
+          sesion_1: "29/10/2024",
+          sesion_2: "5/11/2024",
+          sesion_3: "12/11/2024",
+          sesion_4: "19/10/2024",
+        },
+        contenido_minimo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        id: "M-4493-M3",
+        nombre: "Formación Y Evaluación De Desempeño Del Talento En Gestión Por Competencias",
+        docente: "Pedro Perez",
+        facturacion: "0",
+        monto_a_pagar: 3550,
+        hora_inicio:"19:00",
+        hora_fin:"22:00",
+        fecha_de_clases:{
+          sesion_1:"29/10/2024",
+          sesion_2:"5/11/2024",
+          sesion_3:"12/11/2024",
+          sesion_4:"19/10/2024",
+        },
+        contenido_minimo:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      }
+    ]  },
+];
