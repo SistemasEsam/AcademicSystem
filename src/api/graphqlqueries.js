b43a8acf67a31ae1b3c0d55f73b14e806b76cd89
royerangulo <royer.angulo@esam.edu.bo>
2024-10-29 15:01:37 -0400

    subida de cambios

--- src/api/graphqlqueries.js
+++ src/api/graphqlqueries.js
@@ -1,13 +1,22 @@
 import { gql } from "graphql-request";
 
 export const GET_PROGRAM_BY_ID = gql`
-query GetProgramById($id:Int!){programa (id:$id)
-{
-    id,
-    nombre_compuesto,
-    fecha_inicio,
-    fecha_fin,
-    sede{nombre},
-    postgrado{area{nombre}}}
-}
+  query GetProgramById($id: Int!) {
+    programa(id: $id) {
+      id
+      nombre_compuesto
+      fecha_inicio
+      fecha_fin
+      sede {
+        nombre
+      }
+      postgrado {
+        area {
+          nombre
+        }
+      }
+    }
+  }
 `;
+
+import { gql } from "graphql-request";
