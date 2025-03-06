1da739f4858d4a4128014ac728689f9151efc84a
vincent292 <ariasvincent292@gmail.com>
2025-03-06 16:49:25 -0400

    Proyecto deployd en railiwssadddy

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # ----------------------
-# Etapa de Construcción (Node 20)
+# Etapa de Construcción
 # ----------------------
     FROM node:20-alpine AS builder
     WORKDIR /app
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+import node from "@astrojs/node"; // Nombre correcto del paquete
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
+  adapter: node({ // Usa la variable correcta "node"
+    mode: "standalone"
   }),
+  server: {
+    host: true, // Necesario para exponer en Docker
+    port: 4321
+  }
 });
